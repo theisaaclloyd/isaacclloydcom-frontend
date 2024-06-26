53ce87348bf90011a8c06e48379c4b2fee1832c0
Isaac Lloyd <57055268+theisaaclloyd@users.noreply.github.com>
2024-06-25 20:34:12 -0700

    resume updates

--- app/resume/page.js
+++ app/resume/page.js
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function About() {
 	return (
-		<main className="gap-10 flex items-left flex-col text-xl Aller-Light fade-in mx-auto px-24 pt-20 bg-white text-black">
+		<main className="gap-10 flex items-left flex-col text-xl Aller-Light fade-in mx-auto px-24 py-20 bg-white text-black">
 
 			<section className="flex text-center flex-col gap-5 border-b border-gray-800 pb-4">
 				<h1 className="text-5xl Anth-Regular">ISAAC C. LLOYD</h1>
@@ -34,50 +34,79 @@ export default function About() {
 
 			<section className="flex text-left flex-col gap-5">
 				<h1 className="text-2xl font-bold">Education</h1>
-				<div className="flex flex-col text-lg gap-0">
-					<p className=""><a className='italic'>Cedarville University</a>, Cedarville OH</p>
-					<p className="">Freshman, Graduating May 2027</p>
-					<p className="">Major: [Your Major]</p>
+				<div className="text-lg mt-2">
+					<div className="mb-4">
+						<h2 className="font-bold inline">Cedarville University <span className="italic">- Cedarville, OH</span></h2>
+						<p>BS Cyber Operations, BS Computer Science, Minors: Bible</p>
+						<p>GPA: 3.48 - Expected Graduation: May 2027</p>
+					</div>
+					<ul className="list-disc pl-8">
+						<li>Cedarville University is one of 18 schools in America designated for NCAE-CO (Cyber Operations) and NCAE-CD (Cyber Defence) by The National Centers of Academic Excellence in Cybersecurity (NCAE-C).</li>
+						<li>Relevant Coursework: Object-Oriented Programming with C++, Digital Logic Design.</li>
+					</ul>
 				</div>
 			</section>
 
 			<section className="flex text-left flex-col gap-5">
-				<h1 className="text-2xl font-bold">Skills</h1>
-				<ul className="list-disc pl-8 text-lg mt-2">
-					<li>Technical Proficiency: Advanced understanding of electrical, hydraulic, and marine systems.</li>
-					<li>Quality Assurance: Rigorous testing and inspection to ensure peak performance and safety.</li>
-					<li>Innovation and Customization: Custom solutions for unique installation challenges.</li>
-					<li>Project Management: Efficient coordination and completion of long-term projects.</li>
-					<li>Client Relations: Managing expectations with professionalism and grace.</li>
-					<li>Problem-Solving: Resourceful in devising practical solutions with precision.</li>
-					<li>Team Leadership: Effective communication and collaborative work management.</li>
-					<li>Customer Service: Providing high-tier, knowledgeable support to clientele.</li>
-				</ul>
+				<h1 className="text-2xl font-bold">Work history</h1>
+				<div className="text-lg mt-2">
+					<h2 className="font-bold inline">Nordhavn Yachts <span className="italic">- Dana Point, CA</span></h2>
+
+					<p>Commissioning Technician (Aug 2022 - Present)</p>
+					<ul className="list-disc pl-8">
+						<li>Skilled in commissioning luxury yachts by conducting system-wide tests and quality checks, servicing extensive electrical systems, plumbing, bilge systems, and automated controls, while ensuring all systems meet ABYC standards.</li>
+						<li>Demonstrated exceptional problem-solving abilities by designing and implementing creative solutions for custom installations of furniture, appliances, and lighting fixtures, using bespoke methods to adapt to unique yacht designs and client specifications.</li>
+						<li>Worked constructively with a small team of technicians through the multi-month commissioning process to ensure the flawless functionality of every onboard system without external guidance.</li>
+						<li>Experienced in navigating high-pressure environments by delivering exceptional customer facing service to high net worth clients, ensuring their satisfaction throughout the commissioning process.</li>
+					</ul>
+				</div>
+			</section>
+
+			<section className="flex text-left flex-col gap-5">
+				<h1 className="text-2xl font-bold">Achievements</h1>
+				<div className="text-lg mt-2">
+					<h2 className="font-bold inline">Boy Scouts of America - Troop 12 <span className="italic">- San Juan Capistrano, CA</span></h2>
+					<p>Eagle Scout Rank (2015 - 2023)</p>
+					<ul className="list-disc pl-8">
+						<li>Over 150 hours of community service for various Orange County based organizations.</li>
+						<li>Served in 4 important roles in Troop 12, including assistant patrol leader, patrol leader, and assistant senior patrol leader.</li>
+						<li>As patrol leader, led an organized patrol of 20+ boys. Led weekly meetings and planned troop campouts, meal plans, and activities.</li>
+						<li><strong>Eagle Scout Rank</strong> (2023) - Recognized by U.S. House of Representatives, California State Assembly, and California State Senate. Eagle is the highest rank in the scouting program, as it requires a great amount of effort to complete. Only approx. 4% of scouts achieve Eagle.</li>
+					</ul>
+				</div>
+				<div className="text-lg mt-2">
+					<h2 className="font-bold inline">The National Centers of Academic Excellence in Cybersecurity <span className="italic">- Cedarville, OH</span></h2>
+					<p>VIVID Cyber Competition (Apr 2024)</p>
+					<ul className="list-disc pl-8">
+						<li>With three other students on the team, we ranked 23rd nationally, out of 200 teams.</li>
+						<li>Three day CTF competition, required knowledge to implement offensive and defensive cyber techniques.</li>
+					</ul>
+				</div>
 			</section>
 
 
 
 			<section className="flex text-left flex-col gap-5">
 				<h1 className="text-2xl font-bold">Skills</h1>
-				<ul className="list-disc pl-8 text-lg mt-2">
-					<li>Technical Proficiency: Advanced understanding of electrical, hydraulic, and marine systems.</li>
-					<li>Quality Assurance: Rigorous testing and inspection to ensure peak performance and safety.</li>
-					<li>Innovation and Customization: Custom solutions for unique installation challenges.</li>
-					<li>Project Management: Efficient coordination and completion of long-term projects.</li>
-					<li>Client Relations: Managing expectations with professionalism and grace.</li>
-					<li>Problem-Solving: Resourceful in devising practical solutions with precision.</li>
-					<li>Team Leadership: Effective communication and collaborative work management.</li>
-					<li>Customer Service: Providing high-tier, knowledgeable support to clientele.</li>
-				</ul>
-			</section>
-
-
-
-			<section className="flex text-left flex-col gap-5">
-				<h1 className="text-2xl font-bold">Certifications</h1>
-				<p className="text-lg mt-2">
-					--
-				</p>
+				<div className="text-lg mt-2">
+					<h2 className="font-bold">Proficient in:</h2>
+					<ul className="list-disc pl-8">
+						<li>C++</li>
+						<li>Javascript</li>
+						<li>Python</li>
+						<li>Node.js</li>
+						<li>React.js</li>
+						<li>Next.js</li>
+						<li>Git</li>
+						<li>Github</li>
+					</ul>
+				</div>
+				<div className="text-lg mt-2">
+					<h2 className="font-bold">Certifications:</h2>
+					<ul className="list-disc pl-8">
+						<li>ABYC General (In Progress)</li>
+					</ul>
+				</div>
 			</section>
 
 
