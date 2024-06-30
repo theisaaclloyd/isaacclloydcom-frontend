c38c2b59d18903dc9e40795767b096f36092af58
Isaac Lloyd <57055268+theisaaclloyd@users.noreply.github.com>
2024-06-29 22:40:39 -0700

    home page styling

--- app/page.js
+++ app/page.js
@@ -12,12 +12,10 @@ export const metadata = {
 export default function Home() {
 	return (
 		<main className="h-screen flex flex-col">
-			{/* <BasicHeader /> */}
+			<Header/>
 
-			<section className="flex h-full items-center justify-center" id="home" >
-				<div className="flex flex-col md:flex-row w-full md:items-center mx-auto justify-center flex-container" >
-
-					<div className="flex md:pr-8 justify-center image-container">
+			<section className="flex h-full flex-col md:flex-row md:pl-20 items-center justify-center md:justify-normal flex-container" id="home" >
+				<div className="flex justify-center image-container">
 						<Image
 							className="rounded-xl transition-all duration-500 ease-in-out fade-in"
 							src="/assets/images/isaac.png"
@@ -28,7 +26,7 @@ export default function Home() {
 							alt="Isaac Lloyd"
 						/>
 					</div>
-					<FadeIn className="flex pt-5 flex-col items-center text-center md:text-left justify-center md:items-start gap-0">
+					<FadeIn className="flex pt-5 md:pl-8 flex-col text-center md:text-left justify-center items-start gap-0">
 						<div className="text-5xl">Isaac Lloyd</div>
 						<div className="text-xl Gitan-Regular">BS Cyber Operations & BS Computer Science</div>
 						<div className="text-xl text-orange-200 italic Gitan-Regular">Cedarville University &apos;27</div>
@@ -40,6 +38,13 @@ export default function Home() {
 					</FadeIn> */}
 				</div>
 			</section>
+
+
+			<footer className="w-full bg-gray-800 py-4 px-20">
+				<div className="flex flex-col md:flex-row justify-between items-center">
+					<p className="text-lg">© 2024 Isaac Lloyd</p>
+				</div>
+			</footer>
 
 		</main>
 	);
